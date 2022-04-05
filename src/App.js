14d89f2faa01cd6826fc9e71bdc16fd88767ea99
adgsenpai <adgrules@hotmail.com>
2022-04-06 00:11:42 +0200

    First Commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import "./static/css/nucleo-svg.css";
+import "./static/css/nucleo-icons.css";
+import "./static/css/nucleo-svg.css";
+import "./static/css/soft-ui-dashboard.css?v=1.0.3";
+
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+      <br></br>
+      <h5>ADG SDK says hello from React.JS</h5>
+      <br></br>
+      </div>
+    )
+  }
+}
+
+export default App;
